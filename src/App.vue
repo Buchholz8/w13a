e7af531c88447e8cf74da86f3f143f1b65465a23
markb <markbuchholz8@gmail.com>
2023-04-13 20:01:13 -0600

    first

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <main-header></main-header>
+  </div>
+</template>
+
+<script>
+import MainHeader from './components/main_header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
